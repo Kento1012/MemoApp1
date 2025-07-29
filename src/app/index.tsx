00c51ab650774642bf1567349b6e1b236a0b2403
Kento1012 <kento.20soccer14@gmail.com>
2025-07-29 14:49:02 -0700

    Extract the components and make it simple

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,53 +1,18 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Header from '../component/Header';
+import MemoListItem from '../component/MemoListItem';
+import CircleButton from '../component/CircleButton';
 
 const Index = (): React.JSX.Element => {
     return (
         <View style={styles.container}>
-            <View style={styles.header}>
-                <View style={styles.headerInner}>
-                    <Text style={styles.headerTitle}>Memo App</Text>
-                    <Text style={styles.headerLogout}>ログアウト</Text>
-                </View>
-            </View>
-
+            <Header />
             <View>
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2025年07月28日 10:00</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2025年07月28日 10:00</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2025年07月28日 10:00</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
+                 <MemoListItem />
+                 <MemoListItem />
+                 <MemoListItem />
             </View>
-
-
-            <View style={styles.circleButton}>
-                <Text style={styles.circleButtonLabel}>+</Text>
-            </View>
+            <CircleButton>+</CircleButton>
         </View>
     )
 }
@@ -56,71 +21,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#ffffff',
-    },
-    header: {
-        backgroundColor: '#467FD3',
-        height: 104,
-        justifyContent: 'flex-end',
-    }, 
-    headerInner: {
-        alignItems: 'center',
-    },
-    headerLogout: {
-        position: 'absolute',
-        right: 16,
-        bottom: 12,
-        color: 'rgba(255, 255, 255, 0.7)',
-
-    },
-    headerTitle: {
-        marginBottom: 8,
-        fontSize: 22,
-        lineHeight: 32,
-        fontWeight: 'bold',
-        color: '#ffffff',
-    },
-    memoListItem: {
-        backgroundColor: '#ffffff',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingVertical: 16,
-        paddingHorizontal: 19,
-        alignItems: 'center',
-        borderBottomWidth: 1,
-        borderBottomColor: 'rgba(0, 0, 0, 0.15)',
-    },
-    memoListItemTitle: {
-        fontSize: 16,
-        lineHeight: 32,
-    },
-    memoListItemDate: {
-        fontSize: 12,
-        lineHeight: 16,
-        color: '#848484',
-    },
-    memoListItemDeleteButton: {
-        padding: 8,
-    },
-    circleButton: {
-        width: 64,
-        height: 64,
-        borderRadius: 32,
-        backgroundColor: '#467FD3',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        right: 40,
-        bottom: 40,
-        shadowColor: '#000000',
-        shadowOpacity: 0.25,
-        shadowRadius: 8,
-        shadowOffset: { width: 0, height: 8 },
-        elevation: 4,
-    },
-    circleButtonLabel: {
-        color: '#ffffff',
-        fontSize: 40,
-        lineHeight: 40,
     }
 })
 
