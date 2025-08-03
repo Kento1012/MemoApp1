cc5842f240ff0b5ab675830089fd77d8e4491b50
Kento1012 <kento.20soccer14@gmail.com>
2025-08-03 16:02:48 -0700

    履歴を残さない画面遷移とログアウトボタン useNavigationを取得→UseEffectで画面が表示された状態で１度だけ使う(setOption中で使ってタンをタイリングる)

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,9 +1,11 @@
-import { View, StyleSheet } from 'react-native';
-import { router } from 'expo-router';
-import Header from '../../component/Header';
+import { View, Text, StyleSheet } from 'react-native';
+import { router, useNavigation } from 'expo-router';
+import { useEffect } from 'react';
+
 import MemoListItem from '../../component/MemoListItem';
 import CircleButton from '../../component/CircleButton';
 import Icon from '../../component/icon';
+import LogoutButton from '../../component/LogoutButton';
 
 const handlePress = (): void => {
     // 新規作成処理
@@ -11,6 +13,14 @@ const handlePress = (): void => {
 }
 
 const List = (): React.JSX.Element => {
+    const navigation = useNavigation()
+    
+    useEffect(() => {
+        navigation.setOptions({
+        headerRight: () => { return <LogoutButton /> }
+        })
+    }, [])
+
     return (
         <View style={styles.container}>
             <View>
